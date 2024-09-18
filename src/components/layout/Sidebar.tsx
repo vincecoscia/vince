0ea394edf35c64413f5c78542cd983237b3f3129
vincecoscia <xvincentcosciax@gmail.com>
2024-09-18 17:25:39 -0400

    Small fix

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Briefcase, Code2, CalendarDays, FileText, Home } from 'lucide-react';
 import Link from 'next/link';
 import { useTheme } from 'next-themes';
@@ -6,7 +6,11 @@ import { Button } from '../ui/button';
 
 const Sidebar: React.FC = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
     <aside className="w-64 bg-white dark:bg-zinc-950 border-r border-gray-200 dark:border-zinc-700 flex flex-col justify-between">
@@ -50,13 +54,15 @@ const Sidebar: React.FC = () => {
       {/*  */}
       <div className="px-6 py-3 text-gray-700 dark:text-gray-200 w-full justify-center flex flex-col gap-2">
         {/* Go Home Link */}
-        <Link href="/" className="flex items-center bg-zinc-950 text-white rounded-md p-2 hover:bg-zinc-900 dark:hover:bg-zinc-800 ">
+        <Link href="/" className="flex items-center bg-white dark:bg-zinc-950 dark:text-white rounded-md p-2 dark:hover:bg-zinc-800 hover:bg-gray-100 text-black">
           <Home className="h-5 w-5 mr-3" />
           Home
         </Link>
-        <Button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} className="w-full">
-          {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
-        </Button>
+        {mounted && (
+          <Button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} className="w-full">
+            {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+          </Button>
+        )}
       </div>
     </aside>
   );
