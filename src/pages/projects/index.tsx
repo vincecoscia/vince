8d532d69fe93ecc8d7f14590a0e1f5a1d71c6a29
vincecoscia <xvincentcosciax@gmail.com>
2024-09-20 00:07:40 -0400

    Fresh

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next'
-
+import Head from 'next/head'
 const ProjectsPage: NextPage = () => {
   return (
     <>
+          <Head>
+        <title>vincecoscia</title>
+        <meta name="description" content="Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>Projects</h1>
       {/* Add your projects page content here */}
     </>
