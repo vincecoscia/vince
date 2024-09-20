753c62fd417c0f538db67b016b01e6c0c0ab1095
vincecoscia <xvincentcosciax@gmail.com>
2024-09-20 11:39:07 -0400

    More performance

--- src/components/layout/nav/TransitionOverlay.tsx
+++ src/components/layout/nav/TransitionOverlay.tsx
@@ -9,18 +9,20 @@ const TransitionOverlay = ({ isTransitioning, transitionTheme }: {
     <AnimatePresence>
       {isTransitioning && (
         <motion.div
-            className="fixed pointer-events-none z-50 rounded-full"
-          initial={{ scale: 0 }}
-          animate={{ scale: 2 }}
-          exit={{ opacity: 0, transition: { duration: 0.5, ease: 'easeOut' } }}
+            className="fixed pointer-events-none z-50"
+          initial={{ transform: 'scale(0)' }}
+          animate={{ transform: 'scale(2)' }}
+          exit={{ opacity: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
           transition={{ duration: 1, ease: 'easeInOut' }}
           style={{
             backgroundColor: transitionTheme === 'dark' ? 'rgb(9, 9, 11)' : 'rgb(255, 255, 255)',
             width: '100vmax',
             height: '100vmax',
+            borderRadius: '50%',
             top: '-50vmax',
             right: '-50vmax',
             transformOrigin: 'top right',
+            willChange: 'transform, opacity',
           }}
         />
       )}
